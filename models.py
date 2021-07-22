f813ff393c5ce8e28de357d11cfc013819c571c1
m13uz <m.uzun19@imperial.ac.uk>
2021-07-22 04:58:45 +0300

    added model architectures

---
+++ models.py
@@ -0,0 +1,52 @@
+from myimports import *
+
+class model_de(nn.Module):
+    def __init__(self, D_in = 1, D_out=1, H=50):
+        super(model_de, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+        )
+        self.encode_mu = nn.Sequential(
+            nn.Linear(H,D_out)
+        )
+        self.encode_logvar = nn.Sequential(
+            nn.Linear(H,D_out)
+        )
+        
+    def forward(self, x):
+        code = self.fc(x)
+        mu = self.encode_mu(code)
+        logvar = self.encode_logvar(code)
+        
+        return mu, logvar
+    
+#################################################################
+#################################################################
+class model_mcdrop(nn.Module):
+    def __init__(self, D_in = 1, D_out=1, H=50,dropout_rate=0.2):
+        super(model_mcdrop, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(H,D_out)
+        )
+        
+    def forward(self, x):
+        return self.model(x)
+    
+    
+#################################################################
+#################################################################    
+class model_mimo(nn.Module):
+    def __init__(self, M=3, H=50):
+        super(model_mimo, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(M, H),
+            nn.ReLU(),
+            nn.Linear(H,M)
+        )
+        
+    def forward(self, x):
+        return self.model(x)
